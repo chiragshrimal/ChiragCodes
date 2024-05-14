79d4338c719d174ace4300a4ba8695b041b76407
chirag <chirag@chirag.shrimal>
2024-05-14 17:44:19 +0530

    dvd

---
+++ c++/array/print_sprial_order.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+// good question 
+vector<int> sprial_print(vector<vector<int>> &v){
+    int n=v.size();
+    int m=v[0].size();
+    int i=0;
+    int j=0;
+    vector<int> ans;
+    while(i<=n && j<=m){
+        for(int k=j;k<m;k++){
+            ans.push_back(v[i][k]);
+        }
+        i++;
+        for(int k=i;k<n;k++){
+            ans.push_back(v[k][m-1]);
+        }
+        if(i<=n){
+        m--;
+        for(int k=m-1;k>=j;k--){
+            ans.push_back(v[n-1][k]);
+        }
+        }
+        n--;
+        for(int k=n-1;k>=i;k--){
+            ans.push_back(v[k][j]); 
+        }
+        j++;
+    }
+    return ans;
+
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> v;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>v[i][j];
+        }
+    }
+    vector<int> ans=sprial_print(v);
+    for(auto it : ans){
+        cout<<it<<endl;
+    }
+}
--- c++/array/rotate_matrix_90.cpp
+++ c++/array/rotate_matrix_90.cpp
@@ -1,6 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-ratation
+// brute force apporach 
+// time complexity is O(n^2)
+// space complexity is O(n^2)
+vector<vector<int>>  rotation_matrix_brute(vector<vector<int>> &v){
+    int n=v.size();
+    int m=v[0].size();
+    vector<vector<int>> ans(n,vector<int> (m,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            ans[j][m-1-i]=v[i][j];
+        }
+    }
+    return ans;
+
+}
+// optimal apporach 
+// time complexity is O(n^2)
+// space complexity is O(1)
+// apporach is 
+// 1. transpose the matrix
+// and reverse row wise
+void rotation_matrix_optimal(vector<vector<int>> &v){
+    int n=v.size();
+    int m=v[0].size();
+    // transpose of the matrix
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<m;j++){
+            swap(v[i][j],v[j][i]);
+        }
+    }
+    // reverse row wise
+    for(int i=0;i<n;i++){
+        reverse(v[i].begin(),v[i].end());
+    }
+}
 int main(){
     int n, m;
     cin>>n>>m;
@@ -9,5 +43,21 @@ int main(){
         for(int j=0;j<m;j++){
             cin>> v[i][j];
         }
+    }
+    // vector<vector<int>> ans;
+    // ans=rotation_matrix_brute(v);
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cout<<ans[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    rotation_matrix_optimal(v);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
     }
 }
