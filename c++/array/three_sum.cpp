51fb42e5047cbfb2b10e55ed693712410cd5a5d9
chirag <chirag@chirag.shrimal>
2024-05-18 15:32:37 +0530

    vddf

--- c++/array/three_sum.cpp
+++ c++/array/three_sum.cpp
@@ -28,6 +28,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>v[i];
     }
+    cout<<"hello"<<endl;
     set<vector<int>> brute=sum_brute(v);
     for(auto it : brute){
         for(auto k : it){
